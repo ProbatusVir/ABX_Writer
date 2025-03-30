ec32e0355c3f245a3d53129bbb9513c61e5fad2c
Joshua <joshbear367763@gmail.com>
2025-03-30 18:49:30 -0400

    Added documentation, got rid of the display_bits_system function, a display_hex function, and a toggle_endian function I added a little bit of functionality for the compiler to discern what endianness is used at compile time.

--- src/bitwise.c
+++ src/bitwise.c
@@ -12,6 +12,7 @@ char get_hex_rep(const int n)
 	return n + '0' + (n > 9) * ('A' - '9' - 1);
 }
 
+
 int get_bit(const void* data, const size_t n)
 {
 	const size_t index = n / SYSTEM_BITS;
@@ -35,18 +36,54 @@ int get_nibble(const void* data, const size_t n)
 	return result / NIBBLE_PERMUTATIONS | result % NIBBLE_PERMUTATIONS;
 }
 
-void display_bits_system(const void* data, const size_t start, const size_t n)
-{
-	const size_t upper_bound = start + n;
-	for (size_t i = start; i < upper_bound; i++)
-		printf("%d", get_bit(data, i));
-}
-
 void display_bits(const void* data, const size_t start, const size_t n)
 {
 	const ssize_t upper_bound = start + n;
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	for (size_t i = start; i < upper_bound; i++)
+		printf("%d", get_bit(data, i));
+	#else
 	for (ssize_t i = upper_bound; i >= (ssize_t)0; i--)
 		printf("%d", get_bit(data, i));
+	#endif
+}
+
+void display_hex(const void* data, size_t n)
+{
+	#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	const size_t pairs = n / 2;
+	for (int i = 0; i <= pairs; i += 2)
+	{
+		int nibble = get_nibble(data, i + 1);
+		printf("%c", get_hex_rep(nibble));
+		nibble = get_nibble(data, i);
+		printf("%c", get_hex_rep(nibble));
+	}
+	if (n % 2)
+		printf("%c", get_hex_rep(get_nibble(data, n - 1)));
+
+	#else
+	//I have no way of testing this right now.
+	for (int i = 0; i < n; i++)
+	{
+		const int nibble = get_nibble(&n, i);
+		printf("%c", get_hex_rep(nibble));
+	}
+	#endif
+}
+
+void toggle_endian(void* data, const size_t n)
+{
+	const size_t pairs = n / (size_t)2;
+	const size_t n_indices = n - 1;
+	char* byte_array = (char*)data;
+	for (size_t i = 0; i < pairs; i++)
+	{
+		const size_t j = n_indices - i; // I wonder if having two counter would be more efficient somehow.
+		const char tmp = byte_array[i];
+		byte_array[i] = byte_array[j];
+		byte_array[j] = tmp;
+	}
 }
 
 #undef SYSTEM_BITS
--- src/bitwise.h
+++ src/bitwise.h
@@ -5,13 +5,51 @@
 
 #ifndef BITWISE_H
 #define BITWISE_H
-#include <stddef.h>
 
+#ifndef __BYTE_ORDER__
+#error System endianness could not be determined.
+#endif
+
+#include <stddef.h>
+/**
+ * \brief A type-generic function to get the bit at a specified index of an array of bits.
+ * \param data A pointer to any type of data.
+ * \param n The index of the bit to examine.
+ * \return Returns the value of the bit. 1 or 0.
+ */
 int get_bit(const void* data, size_t n);
+/**
+* \brief Similar to get_bit. This type-generic function gets the nibble at a specified index of an array of nibbles.
+* \param data A pointer to any type of data.
+* \param n The index of the nibble to examine.
+* \return The value of the nibble -- (0-15).
+*/
 int get_nibble(const void* data, size_t n);
+/**
+* \brief Turns a nibble into its hex representation.
+* \param n An integer from 0-15.
+* \return The ASCII representation of the hexidecimal representation of the bit. ('0'-'F')
+*/
 char get_hex_rep(int n);
-void display_bits_system(const void* data, size_t start, size_t n);
+/**
+* \brief Displays an array of bits.
+* \param data A pointer to any type of data.
+* \param start The index of the bit to start displaying.
+* \param n The number of bits to display.
+*/
 void display_bits(const void* data, size_t start, size_t n);
+/**
+ * \brief Display an array of nibbles.\n TODO: Maybe add a start thing to this one just like the display_bits
+ * \param data A pointer to any type of data.
+ * \param n The number of bits to display.
+ */
+void display_hex(const void* data, size_t n);
+/**
+ * \brief Toggle the endianness of data
+ * \param data A pointer to any type of data.
+ * \param n The size of the data.
+ */
+void toggle_endian(void* data, size_t n);
 
 #define ITERATIONS(n) (sizeof(n) / 2)
 #define INDICES(n) (sizeof(n) -1)
